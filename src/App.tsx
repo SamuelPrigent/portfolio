7e9f9f31b90dc47c490baa9c210e3021ed487021
Samuel Prigent <samuel.prigent@yahoo.fr>
2024-04-21 13:58:59 +0200

    première version présentable visuellement ?

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,14 @@ import mail from "./assets/mail.svg";
 import leaf from "./assets/leaf.svg";
 import underline from "./assets/underline.svg";
 //
+import react from "./assets/logo-react.svg";
+import tailwind from "./assets/logo-tailwindcss.svg";
+import node from "./assets/logo-node.svg";
+//
+import kasa from "./assets/preview-kasa.png";
+import fifo from "./assets/preview-fifo.png";
+import lespetitsplats from "./assets/preview-lespetitsplats.png";
+//
 
 function App() {
   window.addEventListener("scroll", function () {
@@ -52,24 +60,19 @@ function App() {
           <div className="px-1 py-1 text-[15px] font-[500] text-green-50 hover:cursor-pointer">
             Skills
           </div>
-          <div className="px-1 py-1 text-[15px] font-[500] text-green-50 hover:cursor-pointer">
-            Contact
-          </div>
           <Switch
             size="2"
             variant="surface"
-            className="my-auto rounded-full bg-[#414141]"
+            color="gray"
+            className=" my-auto rounded-full bg-[#414141]"
             defaultChecked
           />
         </div>
       </div>
       {/* Presentation */}
       <div className="m-0 box-border flex min-w-full bg-[#1b1b1f] ">
-        {/* <div className="max-w-[1100px] m-auto flex items-center justify-around px-10"> */}
         <div className="max-w-11/12 m-auto flex items-center justify-around px-10 pt-[35px]">
-          {/* <div className="flex h-[450px] w-full"> */}
           <div className="flex h-[450px] w-full gap-24">
-            {/* <div className="relative my-auto flex w-[750px] flex-col rounded-lg pt-[100px] text-start font-bold "> */}
             <div className="relative my-auto flex flex-col rounded-lg pt-[70px] text-start font-bold ">
               <img
                 className="z-1 absolute left-[0px] top-[45px]"
@@ -95,11 +98,17 @@ function App() {
                 Fullstack
               </div>
               <div className="mt-[22px] flex gap-2 text-[14px]">
-                <Button radius="full" className="greenButton flex gap-2 px-4">
+                <Button
+                  radius="full"
+                  className="greenButton flex h-[38px] gap-2"
+                >
                   <img src={linkedin} height={18} width={18} alt="Code" />
                   LinkedIn
                 </Button>
-                <Button radius="full" className="grayButton flex gap-2 px-4">
+                <Button
+                  radius="full"
+                  className="grayButton flex h-[38px] gap-2"
+                >
                   <img
                     className="whiteSvg"
                     src={github}
@@ -109,7 +118,10 @@ function App() {
                   />
                   Github
                 </Button>
-                <Button radius="full" className="grayButton flex gap-2 px-4">
+                <Button
+                  radius="full"
+                  className="grayButton flex h-[38px] gap-2"
+                >
                   <img
                     className="whiteSvg"
                     src={mail}
@@ -125,6 +137,36 @@ function App() {
               <div className="gradientBorder absolute left-0 right-0 h-full w-full rounded-full"></div>
               <div className="radiantBackground z-10"> </div>
               <div className="textureBackground z-20"></div>
+              {/* react icon */}
+              <div className="badgeIconColor absolute left-[45px] top-[12px]  flex items-center justify-center rounded-full">
+                <img
+                  src={react}
+                  className="greenSvg"
+                  height={25}
+                  width={25}
+                  alt="logo react"
+                />
+              </div>
+              {/* node icon */}
+              <div className="badgeIconColor absolute right-[-8px] top-[72px]  flex items-center justify-center rounded-full">
+                <img
+                  src={node}
+                  className="greenSvg"
+                  height={25}
+                  width={25}
+                  alt="logo react"
+                />
+              </div>
+              {/* tailwind */}
+              <div className="badgeIconColor absolute right-[27px] top-[290px]  flex items-center justify-center rounded-full">
+                <img
+                  src={tailwind}
+                  className="greenSvg"
+                  height={25}
+                  width={25}
+                  alt="logo react"
+                />
+              </div>
               <img
                 src={composant}
                 height={180}
@@ -144,14 +186,64 @@ function App() {
             <div>🚀</div>
           </Badge>
           <div className="grid-cols-custom3 mt-2 grid h-[270px] w-full gap-4 rounded-xl">
-            <div className="rounded-xl border border-[#2d302c]"></div>
-            <div className="rounded-xl border border-[#2d302c]"></div>
-            <div className="rounded-xl border border-[#2d302c]"></div>
+            <div className="group relative overflow-hidden rounded-xl border border-[#2d302c] text-start hover:cursor-pointer">
+              <img
+                className="absolute inset-0 h-full w-full rounded-xl object-cover"
+                src={fifo}
+                alt="screen site fifo"
+              />
+              <div className="absolute inset-0 flex h-full w-full flex-col gap-1 rounded-[11px] bg-[#1f291cc1] p-6 backdrop-blur-sm transition-opacity group-hover:opacity-0">
+                <div className="text-xl font-bold text-green-100">
+                  FIFO : File d'attente prioritaire
+                </div>
+                <div className="text-normal text-gray-300">
+                  Conception d'un système de file d’attente prioritaire (FIFO :
+                  First In First Out), permettant l’ajout d’actions variées,
+                  consommant différents types de crédits. Attention particulière
+                  apporté à la scalabilité.
+                </div>
+              </div>
+            </div>
+
+            <div className="group relative overflow-hidden rounded-xl border border-[#2d302c] text-start hover:cursor-pointer">
+              <img
+                className="absolute inset-0 h-full w-full rounded-xl object-cover"
+                src={kasa}
+                alt="screen site kasa"
+              />
+              <div className="absolute inset-0 flex h-full w-full flex-col gap-1 rounded-[11px] bg-[#1f291cc1] p-6 backdrop-blur-sm transition-opacity group-hover:opacity-0">
+                <div className="text-xl font-bold text-green-100">Kasa </div>
+                <div className="text-normal text-gray-300">
+                  Développement front de l’application avec une intégration
+                  responsive, la mise en place de React Router permettant une
+                  navigation fluide, accompagné d'une interface intuitive.
+                </div>
+              </div>
+            </div>
+
+            <div className="group relative overflow-hidden rounded-xl border border-[#2d302c] text-start hover:cursor-pointer">
+              <img
+                className="absolute inset-0 h-full w-full rounded-xl object-cover"
+                src={lespetitsplats}
+                alt="screen site les petits plats"
+              />
+              <div className="absolute inset-0 flex h-full w-full flex-col gap-1 rounded-[11px] bg-[#1f291cc1] p-6 backdrop-blur-sm transition-opacity group-hover:opacity-0">
+                <div className="text-xl font-bold text-green-100">
+                  Les Petits Plats
+                </div>
+                <div className="text-normal text-gray-300">
+                  Réalisation d’un algorithme de recherche de recettes de
+                  cuisines. Gestion de différents filtres successifs (texte
+                  simple, ingrédients, appareils, ustensiles).
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
+
       {/* Skills */}
-      <div className="m-0 box-border flex min-w-full bg-[#1b1b1f] pb-[70px] pt-[90px]">
+      <div className="m-0 box-border flex min-w-full bg-[#1b1b1f] pb-[70px] pt-[70px]">
         <div className="relative m-auto flex h-auto w-11/12 flex-col gap-5 px-10 ">
           <Badge className="badgeSectionColor start m-auto flex gap-2 rounded-lg px-4 py-2 text-lg font-semibold leading-tight ">
             <div>Skills</div>
