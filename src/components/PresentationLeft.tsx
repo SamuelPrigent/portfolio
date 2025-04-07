e0bcd816a84e496976124263910bd8657719b507
Samuel Prigent <samuel.prigent@yahoo.fr>
2025-04-07 22:02:50 +0200

    update responsive

--- src/components/PresentationLeft.tsx
+++ src/components/PresentationLeft.tsx
@@ -21,42 +21,48 @@ export default function PresentationLeft() {
       >
         Developpeur
       </div>
-      <div
-        className="specialFont z-10 text-[90px] font-semibold leading-tight text-white max-lg:text-center max-lg:text-[80px] max-md:text-[72px] max-sm:text-[62px]"
-      >
+      <div className="specialFont z-10 text-[90px] font-semibold leading-tight text-white max-lg:text-center max-lg:text-[80px] max-md:text-[72px] max-sm:text-[62px]">
         Fullstack.
       </div>
-      <div className="linkPadding items-center pl-1 justify-left mb-2 mt-[20px] flex flex-wrap gap-[16px] gap-y-3 text-[15px] max-lg:justify-center max-lg:px-[16px] max-md:mt-[40px] max-md:grid z-10">
+      <div className="linkPadding justify-left max-lg:m-[30px auto] z-10 mb-2 mt-[20px] flex w-fit flex-wrap items-center gap-[16px] gap-y-3 pl-1 text-[15px] max-lg:mx-[auto] max-lg:justify-center max-lg:px-[16px] max-md:my-[20px] max-md:grid max-md:gap-[30px]">
         {/* New */}
         <Link
           className="iconSocialPresentationDL flex items-center justify-center gap-2 rounded-full font-semibold"
           to="/cv"
         >
-            <div>Download CV</div>
-            <img src={download} alt="icône de téléchargement"/>
+          <div>Download CV</div>
+          <img src={download} alt="icône de téléchargement" />
         </Link>
-
-        <a
-          className="iconSocialPresentation flex items-center justify-center gap-2 rounded-full font-semibold"
-          target="_blank"
-          href="https://www.linkedin.com/in/samuel-prigent/"
-        >
-          <img src={linkedin} height={34} width={34} alt="logo linkedin" />
-        </a>
-        <a
-          className="iconSocialPresentation flex items-center justify-center gap-2 rounded-full font-semibold"
-          target="_blank"
-          href="https://github.com/SamuelPrigent?tab=repositories"
-        >
-          <img src={github} height={34} width={34} alt="logo linkedin" />
-        </a>
-        <a
-          className="iconSocialPresentationMail iconSocialPresentation flex items-center justify-center gap-2 rounded-full font-semibold"
-          target="_blank"
-          href="mailto:samuel.prigent@yahoo.fr"
-        >
-          <img className="invert" src={mail} height={25} width={25} alt="logo linkedin" />
-        </a>
+        <div className="flex gap-[16px] gap-y-3 max-md:m-auto">
+          <a
+            className="iconSocialPresentation flex items-center justify-center gap-2 rounded-full font-semibold"
+            target="_blank"
+            href="https://www.linkedin.com/in/samuel-prigent/"
+          >
+            <img src={linkedin} height={34} width={34} alt="logo linkedin" />
+          </a>
+          <a
+            className="iconSocialPresentation flex items-center justify-center gap-2 rounded-full font-semibold"
+            target="_blank"
+            href="https://github.com/SamuelPrigent?tab=repositories"
+          >
+            <img src={github} height={34} width={34} alt="logo linkedin" />
+          </a>
+          <a
+            //   className="m-auto"
+            className="iconSocialPresentationMail iconSocialPresentation flex items-center justify-center gap-2 rounded-full font-semibold"
+            target="_blank"
+            href="mailto:samuel.prigent@yahoo.fr"
+          >
+            <img
+              className="invert"
+              src={mail}
+              height={25}
+              width={25}
+              alt="logo linkedin"
+            />
+          </a>
+        </div>
       </div>
     </div>
   );
