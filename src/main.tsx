ffe3d26021ddcdfdd689427eb3be9fbaf82215da
Samuel Prigent <samuel.prigent@yahoo.fr>
2025-10-10 14:19:59 +0200

    update base & config

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,8 @@ import BackgroundGrid from "./components/BackgroundGrid.tsx";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter
-      basename="/samuelprigent/"
+      //   basename="/samuelprigent/"
+      basename="/"
       future={{ v7_relativeSplatPath: true, v7_startTransition: true }}
     >
       <BackgroundGrid />
